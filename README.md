9fa9cf029e85e91f558946d52d7dfe944155bac4
Ivan Orellana Jr <iorellana21@gmail.com>
2021-02-19 20:45:31 -0500

    testing appending name to README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # readme-generator
 README Generator
+ivan
--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("are you there?");
+// inquirer module
+const inquirer = require("inquirer");
+// module for files
+const fs = require("fs");
+
+
+
+// BASICS - capture user input by calling inquirer
+inquirer.prompt([
+    {
+        type: 'confirm',
+        message: 'Do you want to work on your README?',
+        default: true,
+        name: 'begin'
+
+    },
+    {
+        type: 'input',
+        message: 'what is your name?',
+        name: 'username'
+    },
+    {
+        type: 'list',
+        message: `hi how are you?`,
+        choices: ["Im good thanks", "Im great thanks", "thanks"],
+        name: 'feeling'
+    }
+
+]).then(response => {
+    // display user input
+    console.log(response);
+    console.log(response.begin);
+    console.log(response.username);
+    console.log(response.feeling);
+
+    // append data to README file using fs module
+    fs.appendFile("README.md", response.username, (err) =>
+        err ? console.error(err) : console.log("pushed to README")
+    );
+
+
+});
+
