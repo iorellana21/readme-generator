aeb3e9dd5a2c787693f615bcb47aa7dbf7e5b1a7
Ivan Orellana Jr <iorellana21@gmail.com>
2021-02-20 16:44:12 -0500

    updating index.js and README

---
+++ README.md
@@ -0,0 +1,25 @@
+# README Generator
+## Description
+The README Generator app is intended to be used for users to have a README file generated for them after answering prompts via NODE.JS.
+## What Was Done
+* Build index.js file
+* Use Inquirer module
+* Generate README file with user answers
+---
+## Process
+* User executes 'node index' to run README Generator program.
+* User answers prompts to gather information for README file
+* When all prompts are answers, README file is generated for user
+
+## Screencastify link
+* video recording
+---
+## Credits
+* [NPM](https://www.npmjs.com/)
+* [Stack Overflow](https://stackoverflow.com/)
+* [W3Schools](https://www.w3schools.com/)
+* [Google](https://www.google.com/)
+---
+## Links
+### GitHub URL
+* https://github.com/iorellana21/readme-generator
--- index.js
+++ index.js
@@ -5,16 +5,50 @@ const fs = require("fs");
 // validate email
 var validateEmail = require("email-validator");
 
-
-
 // BASICS - capture user input by calling inquirer
 inquirer.prompt([
-    {
-        type: 'confirm',
-        message: 'Do you want to create a README for your project?',
-        default: true,
-        name: 'begin'
+    // {
+    //     type: 'confirm',
+    //     message: 'Do you want to create a README for your project?',
+    //     default: true,
+    //     name: 'begin'
 
+    // },
+    {
+        type: 'input',
+        message: 'what is the title of your project?',
+        name: 'project'
+    },
+    {
+        type: 'input',
+        message: 'Provide your project Description:',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What Installations are needed to run this program?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Provide examples of Usage:',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Provide Test Instructions:',
+        name: 'test'
+    },
+    {
+        type: 'list',
+        message: 'Which license do you want to use?',
+        choices: ["IBM", "MIT"],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Provide Contributors:',
+        name: 'contributors'
     },
     {
         type: 'input',
@@ -25,47 +59,25 @@ inquirer.prompt([
         type: 'input',
         message: 'What is your email?',
         name: 'email'
-    },
-    {
-        type: 'input',
-        message: 'what is the title of your project?',
-        name: 'project'
-    },
-    {
-        type: 'input',
-        message: 'Provide your project Description: ',
-        name: 'description'
-    },
-    {
-        type: 'input',
-        message: 'What Installations are needed to run this program?',
-        name: 'installation'
-    },
-    {
-        type: 'list',
-        message: 'Which license do you want to use?',
-        choices: ["IBM", "MIT"],
-        name: 'license'
-    },
-    {
-        type: 'input',
-        message: 'Provide Contributors: ',
-        name: 'contributors'
     }
 ]).then(response => {
     // display user input
     console.log(response);
 
     // push data to README-GEN file
-    fs.appendFile(
+    fs.writeFile(
         // file name
         "README-GEN.md",
         // data
-        `# ${response.project} \n ${response.description} \n` +
-        `## Installation \n ${response.installation} \n` +
+        `# ${response.project} \n` + 
+        `## Table of Contents \n * Description \n * Installation \n * Usage \n * Testing \n * License \n * Contributors \n * Questions \n` +
+        `## Description \n ${response.description} \n` +
+        `## Installation Instructions \n ${response.installation} \n` +
+        `## Usage \n ${response.usage} \n` +
+        `## Testing Instructions \n ${response.test} \n` +
         `## License \n ${response.license} \n` +
         `## Contributors \n ${response.contributors} \n` +
-        `Github email: ${response.email} \n \n`,
+        `## Questions \n * GitHub username: ${response.username} \n * GitHub email: ${ response.email } \n`,
         // callback
         (err) =>
         err ? console.error(err) : console.log("Boom! Created README")
